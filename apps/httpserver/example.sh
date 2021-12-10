c3d2a95b2904f24d20eb001d47ab44a62adc4800
Uwe Eisele <code@uweeisele.eu>
2021-12-10 14:23:10 +0100

    [app] docker-compose file

--- apps/httpserver/example.sh
+++ apps/httpserver/example.sh
@@ -1,35 +1,36 @@
 #!/usr/bin/env bash
 
-PORT=${PORT:-3000}
+TARGET_HOST=${TARGET_HOST:-localhost}
+TARGET_PORT=${TARGET_PORT:-3000}
 
 echo -------------------------------------------------
 echo - fetching a missing object
 echo -------------------------------------------------
-curl -i http://localhost:${PORT}/api/task/9999 
+curl -i http://${TARGET_HOST}:${TARGET_PORT}/api/task/9999 
 echo
 echo -------------------------------------------------
 echo - posting an object
 echo -------------------------------------------------
-curl -i -X POST http://localhost:${PORT}/api/task/1111 --header "Content-Type: application/json"  --data '{"assignees":["tom","joe"],"status":{"user":"tom","type":"CREATED"}}' 
+curl -i -X POST http://${TARGET_HOST}:${TARGET_PORT}/api/task/1111 --header "Content-Type: application/json"  --data '{"assignees":["tom","joe"],"status":{"user":"tom","type":"CREATED"}}' 
 echo
 echo -------------------------------------------------
 echo - posting an object
 echo -------------------------------------------------
-curl -i -X POST http://localhost:${PORT}/api/task/2222 --header "Content-Type: application/json"  --data '{"assignee":"bob"}' 
+curl -i -X POST http://${TARGET_HOST}:${TARGET_PORT}/api/task/2222 --header "Content-Type: application/json"  --data '{"assignee":"bob"}' 
 echo
 echo -------------------------------------------------
 echo - getting the posted object 1111
 echo -------------------------------------------------
-curl -i http://localhost:${PORT}/api/task/1111 
+curl -i http://${TARGET_HOST}:${TARGET_PORT}/api/task/1111 
 echo
 echo -------------------------------------------------
 echo - getting the posted object 2222
 echo -------------------------------------------------
-curl -i http://localhost:${PORT}/api/task/2222 
+curl -i http://${TARGET_HOST}:${TARGET_PORT}/api/task/2222 
 echo
 echo -------------------------------------------------
 echo - getting all posted objects
 echo -------------------------------------------------
-curl -i http://localhost:${PORT}/api/task 
+curl -i http://${TARGET_HOST}:${TARGET_PORT}/api/task 
 echo
 echo -------------------------------------------------
