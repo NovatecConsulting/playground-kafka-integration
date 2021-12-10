848d57741d1beb50db348598b09f57a5aeec1e93
Uwe Eisele <code@uweeisele.eu>
2021-12-10 13:25:03 +0100

    [app-httpserver] mock db implementation

---
+++ apps/httpserver/example.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+echo -------------------------------------------------
+echo - fetching a missing object
+echo -------------------------------------------------
+curl -i http://localhost:3000/api/task/9999 
+echo
+echo -------------------------------------------------
+echo - posting an object
+echo -------------------------------------------------
+curl -i -X POST http://localhost:3000/api/task/1111 --header "Content-Type: application/json"  --data '{"assignees":["tom","joe"],"status":{"user":"tom","type":"CREATED"}}' 
+echo
+echo -------------------------------------------------
+echo - posting an object
+echo -------------------------------------------------
+curl -i -X POST http://localhost:3000/api/task/2222 --header "Content-Type: application/json"  --data '{"assignee":"bob"}' 
+echo
+echo -------------------------------------------------
+echo - getting the posted object 1111
+echo -------------------------------------------------
+curl -i http://localhost:3000/api/task/1111 
+echo
+echo -------------------------------------------------
+echo - getting the posted object 2222
+echo -------------------------------------------------
+curl -i http://localhost:3000/api/task/2222 
+echo
+echo -------------------------------------------------
+echo - getting all posted objects
+echo -------------------------------------------------
+curl -i http://localhost:3000/api/task 
+echo
+echo -------------------------------------------------
--- apps/httpserver/src/server.ts
+++ apps/httpserver/src/server.ts
@@ -6,9 +6,67 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(express.json())
+
 app.get('/', (req: any, res: any) => {
-	res.send('Hello remote world!\n');
+	res.send('Mock HTTP Server!\n');
 });
+
+app.post("/api/:group/:id", (req: express.Request, res: express.Response) => {
+	const group: string = req.params.group
+	const id: string = req.params.id
+	insertInto(group, id, req.body)
+	res.send("ok")
+})
+
+app.get("/api/:group/:id", (req: express.Request, res: express.Response) => {
+	const group: string = req.params.group
+	const id: string = req.params.id
+	const record = getByGroupAndId(group, id)
+	if (record) {
+		res.json(record)
+	} else {
+		res.status(404).send('not found')
+	}
+})
+
+app.get("/api/:group", (req: express.Request, res: express.Response) => {
+	const group: string = req.params.group
+	const entities = getAllByGroup(group)
+	res.json({items: entities})
+})
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+// DB
+type Record = {id: string, createdAt: string, updatedAt: string}
+const DB: Map<string,Map<string,Record>> = new Map<string,Map<string,Record>>();
+
+function insertInto(group: string, id: string, record: {}) {
+	const createdAt = getByGroupAndId(group, id)?.createdAt || new Date().toISOString()
+	const updatedAt = new Date().toISOString()
+	const recordToInsert = Object.assign({id: id, createdAt: createdAt, updatedAt: updatedAt}, record)
+	groupDBOf(group).set(id, recordToInsert)
+}
+
+function getByGroupAndId(group: string, id: string): Record | null {
+	const record = groupDBOf(group).get(id)
+	if (record !== undefined) {
+		return record
+	}
+	return null
+}
+
+function getAllByGroup(group: string): Record[] {
+	return Array.from(groupDBOf(group).values())
+}
+
+function groupDBOf(group: string): Map<string,Record> {
+	let groupDB = DB.get(group)
+	if (!groupDB) {
+		groupDB = new Map<string,Record>()
+		DB.set(group, groupDB)
+	}
+	return groupDB
+}
